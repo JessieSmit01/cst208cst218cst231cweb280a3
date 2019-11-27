835546271da0b1f4b2ec22714a05a8cd40ae80b4
Tara Epp <CST208@cst.siast.sk.ca>
2019-11-27 08:25:11 -0600

    Initial push to master: creating files -Tara

---
+++ Vehicle.php
@@ -0,0 +1,10 @@
+<?php
+/**************************************
+ * File Name: Vehicle.php
+ * User: cst208 Tara Epp, cst218 Carson Kearns, Jessie Smith
+ * Date: 2019-11-11/27/2019
+ * Project: CWEB280 A3
+ *
+ * this is just a copy of the Vehicle.php file you create the lib folder
+ *
+ **************************************/
