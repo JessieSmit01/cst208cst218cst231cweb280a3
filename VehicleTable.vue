1c4d6129a77fbe553daba256c5fbe0fdddefa55f
Tara Epp <cst208@CST.SIAST.SK.CA>
2019-12-02 12:56:41 -0600

    search is working, added more comments.

--- VehicleTable.vue
+++ VehicleTable.vue
@@ -1,39 +1,87 @@
 <template>
-    <!--    /* Assign to: Tara */-->
-
-    <!--    //contains a b-table that lists and sorts an array of vehicles-->
-
-    <!--    //8.	Create a Vue single file component which uses b-table - see https://bootstrap-vue.js.org/docs/components/table/#table-body-transition-support-->
-    <!--    // See Complete Example (right side menu)-->
-    <!--    //9.	Must use a promise based provider (that uses axios to load the table) with :items-->
-    <!--    //10.	Allow for local sorting on all fields - ( use no-provider-sorting option)-->
-    <!--    //11.	Add an ‘actions’ column with an edit button that opens the VehicleInput modal with the form inputs filled with the corresponding vehicle data-->
     <div>
+
+        <b-col lg="6">
+            <b-form-group
+                    label="Filter"
+                    label-cols-sm="3"
+                    label-align-sm="right"
+                    label-size="sm"
+                    label-for="filterInput"
+            >
+                <b-input-group size="sm">
+                    <!--
+                        FILTERING REFERENCE: https://bootstrap-vue.js.org/docs/components/table/#debouncing-filter-criteria-changes <-- but mostly used the "complete" example
+                        - set a debounce like we did in class so it doesn't hit the API too hard by searching after every modification
+                        - model the search field on the filter property,
+                        - it is a search type input,
+                        - give it a placeholder,
+                        - and match the id with the label
+                    -->
+                    <b-form-input
+                            debounce=500
+                            v-model="filter"
+                            type="search"
+                            id="filterInput"
+                            placeholder="Search"
+                    ></b-form-input>
+                    <!--
+                        tack on a button after the search bar
+                        clicking on it sets the filter to empty string,
+                        and it is disabled when there is no
+                     -->
+                    <b-input-group-append>
+                        <b-button :disabled="!filter" @click="filter = ''" variant="primary">Clear</b-button>
+                    </b-input-group-append>
+                </b-input-group>
+            </b-form-group>
+        </b-col>
+
+
+<!--        TODO: this button is not going to stay here. TESTING PURPOSES ONLY -->
         <b-button @click="refresh">Refresh Table</b-button>
+        <!-- TABLE PROPERTIES: all from https://bootstrap-vue.js.org/docs/components/table
+            ref: so the refresh method knows what to refer to,
+            striped: cosmetic, makes it look nice, hover: gives it a little styling change on hover
+            head-variant: looks nice, contrast!
+            no-provider-sorting: enables local sorting
+            primary-key: tell it the primary key so it can optimize rendering (else it uses the row index number that changes when sorting
+            transition-props: tell it which bootstrap transition to use
+            fields: which columns to show
+            isBusy: link to boolean indicating if the data ia still loading
+            items: link to the provider to tell it to get the data from there
+         -->
         <b-table
-                id="vehicleTable"
+                ref="table"
                 striped
                 hover
                 head-variant="dark"
-                sticky-header="40%"
                 no-provider-sorting
                 primary-key="vehicleID"
                 :tbody-transition-props="transProps"
                 :fields="fields"
+                :filter="filter"
                 :busy="isBusy"
                 :items="provider"
         >
-            <!-- A custom formatted header cell for the actions column -->
+            <!-- A custom formatted header cell for the actions column
+                the "head" links to the appropriate field, "data" to the table's data, click to the add method, rest is formatting
+                names of colour variants: https://bootstrap-vue.js.org/docs/reference/color-variants/
+            -->
             <!-- https://bootstrap-vue.js.org/docs/components/table/#header-and-footer-custom-rendering-via-scoped-slots -->
             <template v-slot:head(actions)="data" >
                 <b-button
                         @click="add"
                         class="fas fa-plus"
+                        title="Add"
                         variant="success"
                 ></b-button>
             </template>
 
-            <!-- A virtual column for the "actions"-->
+            <!-- A virtual column for the "actions"
+                has the same links as the header to the field and the data
+                button sends the particular data item over to the edit method
+            -->
             <template v-slot:cell(actions)="data">
                 <b-button
                         @click="edit(data.item)"
@@ -43,10 +91,12 @@
                 ></b-button>
             </template>
 
-            <!-- a spinner! This is shown when the table is in the "busy" state, ie: when it is fetching data-->
+            <!-- a spinner! This is shown when the table is in the "busy" state, ie: when it is fetching data
+                It looks at the busy status of the table, and shows it instead of the table data if it is busy
+            -->
             <!-- https://bootstrap-vue.js.org/docs/components/table/#table-busy-state-->
             <template v-slot:table-busy>
-                <div class="text-center text-danger my-2">
+                <div class="text-center text-danger">
                     <b-spinner class="align-middle"></b-spinner>
                     <strong>Loading...</strong>
                 </div>
@@ -58,20 +108,19 @@
 </template>
 
 <script>
+
     module.exports = {
-        name: "VehicleTable",
-        data() {
+        name: "VehicleTable", //how to refer to this component
+        data() { //all of the data belonging to the table
             return {
                 //https://bootstrap-vue.js.org/docs/components/table/#automated-table-busy-state
                 //do not actually need to set this anywhere when using a provider
                 isBusy: false, //is true when the data is loading.
-                transProps: {
-                    // Transition name
-                    //https://bootstrap-vue.js.org/docs/components/table/#table-body-transition-support
-                    //fancy transition property requires the name of the transition, a css style telling it what to do, and for the primary key to be set
+                transProps: { //set the name of the table transition
                     name: 'flip-list'
                 },
-                fields: [
+                filter: '', //the search string to filter on
+                fields: [ //the fields to display
                     {
                         key: 'vehicleID',
                         label: 'ID',
@@ -95,31 +144,31 @@
                     },
                     {
                         key: 'actions',
-                        label: 'icon here',
-                        // variant: 'dark'
+                        label: 'icon here'
                     }
                 ]
             }
         },
         methods: {
+
             /**
-             *
+             * makes the axios call
+             * returns in the form of a promise, which says, "the data is on the way",
+             * which when it resolves, will return the data from the response (which is then set to the items)
              * @returns {Promise<any> | Promise<T | Array>}
              */
-            provider(ctx) {
+            provider() {
                 //The provider automatically handles the isBusy
                 //(ie: sets it true at the beginning of the method, and false at the end), so no need to directly code it
-                //we are busy until we get the data back
-                // this.isBusy = true;
 
                 //create a promise that is the axios get method to the api. No search currently implemented, so we are not sending in a search term
-                let promise = axios.get('vehicle-api.php', {params: {searchfor:''}});
+                let promise = axios.get('vehicle-api.php', {params: {searchfor:this.filter}});
 
                 //return the promise where the response is what returns from the API, or empty array on error
                 return promise.then(response => {
                     //on success, return the data from the response to be set to the items
                     return response.data;
-                    // return(items)
+
                 }).catch(errors => {
                     //TODO: Probably do some error handling
                     console.log(errors);
@@ -142,9 +191,15 @@
             add() {
                 this.$emit('add');
             },
+
+            /**
+             * forces a refresh of the table data
+             * looks at the refs, which we have set tha table as "table" so it refreshes that item
+             * https://bootstrap-vue.js.org/docs/components/table/#force-refreshing-of-table-data
+             */
             refresh()
             {
-                console.log("Refresh");
+                this.$refs.table.refresh();
             }
         }
 
